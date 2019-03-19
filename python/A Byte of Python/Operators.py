a1cadea34d0fb392c82a5bffd184daf75e3b5b3a
amvicc <amvicc@outlook.com>
2019-03-19 20:54:11 +0300

    Continue learning python

---
+++ python/A Byte of Python/Operators.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Learning to use operators in python
+
+print('Using Plus:')
+print(2 + 3)
+print('Left part' + ' + ' + 'Right part')
+
+print('Using Minus:')
+print(50 - 24)
+
+print('Using Multiplying:')
+print(13 * 2)
+print('Ups ' * 3)
+
+print('Using Power:')
+print(2 ** 9)
+
+print('Using Dividing:')
+print(5 / 3)
+print(5 // 3)
+print(5 % 3)
+
+print('Using Bit operations:')
+print(2 << 4)  # Left shift
+print(8 >> 2)  # Right shift
+print(6 & 2)  # Bit-wise AND
+print(6 | 2)  # Bit-wise OR
+print(6 ^ 2)  # Bit-wise XOR
+print(~6)  # Bit-wise invert
