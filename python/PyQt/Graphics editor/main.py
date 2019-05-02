dff78d06d714e9279aac5164caaf7d5787711e45
amvicc <amvicc@outlook.com>
2019-05-02 21:51:06 +0300

    Creating a Graphics editor project Adding open picture

---
+++ python/PyQt/Graphics editor/main.py
@@ -0,0 +1,41 @@
+import sys
+import cv2
+import MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Window(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        self.setupUi(self)
+
+        self.scene = QtWidgets.QGraphicsScene()
+        self.graphicsView.setScene(self.scene)
+
+        self.actionOpen.triggered.connect(self.openPicture)
+
+    def openPicture(self):
+        self.scene.clear()
+
+        filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Open picture')
+        img = cv2.imread(filename[0])
+        height, width, byteValue = img.shape
+        byteValue = byteValue * width
+
+        cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
+
+        qImg = QtGui.QImage(img, width, height, byteValue, QtGui.QImage.Format_RGB888)
+        pixMap = QtGui.QPixmap.fromImage(qImg)
+        self.scene.addPixmap(pixMap)
+        self.graphicsView.fitInView(QtCore.QRectF(0, 0, width, height), QtCore.Qt.KeepAspectRatio)
+        self.scene.update()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = Window()
+    mainWindow.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
