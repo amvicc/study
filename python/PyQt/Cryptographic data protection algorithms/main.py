8eb70d2b403933acf69ed4e70fd3cf964c933af8
amvicc <amvicc@outlook.com>
2019-05-31 15:54:37 +0300

    Connect all Encrypton Windows

--- python/PyQt/Cryptographic data protection algorithms/main.py
+++ python/PyQt/Cryptographic data protection algorithms/main.py
@@ -2,8 +2,10 @@ import sys
 import MainWindow
 from task1 import Task1
 from task2 import Task2
+from task9 import Task9
 from task10 import Task10
 from task11 import Task11
+from task12 import Task12
 from PyQt5 import QtWidgets
 
 
@@ -14,13 +16,17 @@ class MyWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
 
         self.task1_window = Task1()
         self.task2_window = Task2()
+        self.task9_window = Task9()
         self.task10_window = Task10()
         self.task11_window = Task11()
+        self.task12_window = Task12()
 
         self.pbTask1.clicked.connect(self.task1)
         self.pbTask2.clicked.connect(self.task2)
+        self.pbTask9.clicked.connect(self.task9)
         self.pbTask10.clicked.connect(self.task10)
         self.pbTask11.clicked.connect(self.task11)
+        self.pbTask12.clicked.connect(self.task12)
 
     def task1(self):
         self.task1_window.show()
@@ -28,11 +34,17 @@ class MyWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
     def task2(self):
         self.task2_window.show()
 
+    def task9(self):
+        self.task9_window.show()
+
     def task10(self):
         self.task10_window.show()
 
     def task11(self):
         self.task11_window.show()
+
+    def task12(self):
+        self.task12_window.show()
 
 
 def main():
